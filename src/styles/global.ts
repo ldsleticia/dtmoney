491a1fb2554c48c9027f21d9210756233b38d002
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-05-16 20:42:22 -0300

    incluindo estilos

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,15 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 :root {
     --background: #f0f2f5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --shape: #ffffff
 
 }
 
@@ -17,6 +26,24 @@ body {
     -webkit-font-smoothing: antialiased;
 }
 
+button {
+    cursor: pointer;
+}
 
+/* quando o botão estiver desabilitado, ficará com uma "plaquinha"
+ informando que não pode ser clicado naquele momento */
+[disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+}
+
+html {
+    @media (max-width : 1080px) {
+        font-size: 93.75%;
+    }
+    @media (max-width: 720px) {
+        font-size: 87.5%;
+    }
+}
 
 `;
