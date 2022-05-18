0bd43ac173839573dc312abc2de46846db50ce96
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-05-17 21:59:37 -0300

    inserindo header

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -46,4 +46,13 @@ html {
     }
 }
 
+body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+}
+
+h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+}
+
 `;
