4e7b4ae5d11ecd1e8204dc89eea26e95db94c386
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-05-18 21:27:23 -0300

    inclusão da table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransationsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
     return (
         <Container>
             <Summary/>
+            <TransationsTable />
         </Container>
     )
 }
