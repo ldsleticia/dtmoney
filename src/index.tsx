24f3e195e84a693d761d33c7d1528bdcd02f3619
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-05-18 22:09:45 -0300

    inclusão do mirage js e da biblioteca react-modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from "miragejs"
 import App from './App';
+
+
+createServer({
+  routes() {
+    this.namespace = 'api' 
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date() 
+        }
+      ]
+    })
+    // as apis estão a partir da rota api
+    // porque colocamos no TransactionsTable
+    // que seria a partir dessa rota
+    // e vamos direcionar para o mirage.js
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
