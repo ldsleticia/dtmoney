fa47ee5aa661f503703cf0faea1c7d235d298bbe
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-05-19 23:21:10 -0300

    uso da biblioteca react modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpemNewTransactionModal: ( ) => void
+}
+
+export function Header({onOpemNewTransactionModal}: HeaderProps) {
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpemNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
